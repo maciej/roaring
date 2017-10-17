8650a705d2e3c50a90d47349e25e71520891ac7f
Maciej Bilas <m.bilas@adform.com>
2017-10-17 16:18:19 +0200

    determineOutputContainerCount WIP

--- parallel.go
+++ parallel.go
@@ -5,7 +5,7 @@ import "runtime"
 type parOp int
 
 const (
-	parOpAnd parOp = iota
+	parOpAnd    parOp = iota
 	parOpOr
 	parOpXor
 	parOpAndNot
@@ -76,6 +76,72 @@ type parResult struct {
 	container container
 	empty     bool
 }
+
+// TODO add tests
+func determineOutputContainerCount(x1, x2 *Bitmap, operation parOp) uint16 {
+	count := uint16(0)
+	pos1 := 0
+	pos2 := 0
+	length1 := x1.highlowcontainer.size()
+	length2 := x2.highlowcontainer.size()
+
+	switch operation {
+	case parOpAnd:
+		for pos1 < length1 && pos2 < length2 {
+			key1 := x1.highlowcontainer.getKeyAtIndex(pos1)
+			key2 := x2.highlowcontainer.getKeyAtIndex(pos2)
+
+			if key1 == key2 {
+				pos1++
+				pos2++
+				count++
+			} else if key1 < key2 {
+				pos1 = x1.highlowcontainer.advanceUntil(key2, pos1)
+			} else {
+				pos2 = x2.highlowcontainer.advanceUntil(key1, pos2)
+			}
+		}
+	case parOpAndNot:
+		for pos1 < length1 && pos2 < length2 {
+			key1 := x1.highlowcontainer.getKeyAtIndex(pos1)
+			key2 := x2.highlowcontainer.getKeyAtIndex(pos2)
+
+			if key1 == key2 {
+				pos1++
+				pos2++
+				count++
+			} else if key1 < key2 {
+				pos1 = x1.highlowcontainer.advanceUntil(key2, pos1)
+			} else {
+				pos2 = x2.highlowcontainer.advanceUntil(key1, pos2)
+				count++
+			}
+		}
+		count += uint16(length1)
+	default:
+		for pos1 < length1 && pos2 < length2 {
+			key1 := x1.highlowcontainer.getKeyAtIndex(pos1)
+			key2 := x2.highlowcontainer.getKeyAtIndex(pos2)
+
+			if key1 == key2 {
+				pos1++
+				pos2++
+				count++
+			} else if key1 < key2 {
+				count++
+				pos1++
+			} else {
+				count++
+				pos2++
+			}
+		}
+		count += uint16((length2 - pos2) + (length1 - pos1))
+	}
+
+	return count
+}
+
+func (aggregator ParAggregator) walkBitmapPair()
 
 func (aggregator ParAggregator) And(x1, x2 *Bitmap) *Bitmap {
 	answer := NewBitmap()
